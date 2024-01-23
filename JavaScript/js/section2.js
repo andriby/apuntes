322e8425fd531cb59491932c045ac08a00a767ba
Adrian <adrian.dryzara3@gmail.com>
2024-01-22 23:11:42 -0400

    clase 03

--- JavaScript/js/section2.js
+++ JavaScript/js/section2.js
@@ -35,3 +35,60 @@ function recursiva(){
     alert("Hola desde una funcion recursiva")
     recursiva()
 }
+
+// Funciones con retorno de valores
+
+function retornar(condicion = false){
+    let mensaje = "Hola desde retornar"
+    if (condicion) {
+        return mensaje
+    } else {
+        return "La condicion no se cumple"
+    }
+}
+
+console.log(retornar(true))
+
+// Alcance de variables
+
+if (true) {
+    /* Variables locales: solo funcionan dentro del bloque de codigo donde fue declarado */
+    let variable1 = "Hola"
+    /* Variables globales: se pueden utilizar en cualquier parte del codigo */
+    var variable2 = "Hola2"
+}
+
+console.log( variable2)
+
+// Alcance de funciones
+
+function alcance(){
+    var prueba_alcance = "hola"
+    return (prueba_alcance)
+}
+
+// Ejecucion 
+console.log(alcance())
+console.log(alcance)
+
+// Metodos de cadenas de texto (strings)
+let cadena = "Hola mundo mundo"
+// Longitud de la cadena
+console.log(cadena.length)
+// Transformar a mayusculas
+console.log(cadena.toUpperCase())
+// Transformar a minusculas
+console.log(cadena.toLowerCase())
+// Obtener caracter de una cadena
+console.log(cadena.charAt(5))
+// Verificar si una cadena contiene un texto
+console.log(cadena.includes("Mundo"))
+// Reemplazar texto de una cadena
+console.log(cadena.replace("mundo", "a todos"))
+/* cadena =  cadena.replace("mundo", "a todos")*/
+
+// Cortar un string 
+console.log(cadena.slice(0, 4))
+
+// Cortar un string 
+console.log(cadena.substring(0, 4))
